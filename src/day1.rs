a701e79a4e6e271c98f0ff8f823362558c020e69
Eric Frees <efrees@atlasrfid.com>
2018-12-03 11:10:40 -0600

    Add basic module organization

---
+++ src/day1.rs
@@ -0,0 +1,56 @@
+use std::collections::*;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn solve() {
+    println!("Day 1");
+
+    let filename = "day1input.txt";
+
+    // TODO: factor out function
+    let filename = "inputs/".to_owned() + filename;
+    let mut f = File::open(filename).expect("File not found.");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Problem reading the contents of the file.");
+
+    let mut total = 0;
+    let mut seen = HashSet::new();
+    seen.insert(total);
+    let mut first_repeated = 0;
+    let mut searching = true;
+
+    let lines = contents.split_whitespace();
+    let int_parser = |x: &str| x.parse::<i32>().unwrap();
+    let diffs = lines.map(int_parser);
+    for diff in diffs {
+        total += diff;
+
+        if searching && seen.contains(&total) {
+            searching = false;
+            first_repeated = total;
+        } else {
+            seen.insert(total);
+        }
+    }
+
+    println!("Total: {}", total);
+
+    while searching {
+        let lines = contents.split_whitespace();
+        let diffs = lines.map(int_parser);
+        for diff in diffs.into_iter() {
+            total += diff;
+
+            if searching && seen.contains(&total) {
+                searching = false;
+                first_repeated = total;
+                break;
+            }
+
+            seen.insert(total);
+        }
+    }
+
+    println!("First repeated: {}", first_repeated);
+}
---
+++ src/day3.rs
@@ -0,0 +1,62 @@
+use regex::Regex;
+use std::collections::*;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn solve() {
+    println!("Day 3");
+
+    let mut grid = vec![[0; 1000]; 1000];
+    let mut last_drawn = vec![[0; 1000]; 1000];
+    let mut non_overlapped_ids = HashSet::<i32>::new();
+
+    let filename = "day3input.txt";
+
+    let filename = "inputs/".to_owned() + filename;
+    let mut f = File::open(filename).expect("File not found.");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Problem reading the contents of the file.");
+
+    let lines = contents.lines();
+    let re = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
+
+    for line in lines {
+        let cap = re.captures_iter(line).next().unwrap();
+        let id: i32 = cap[1].parse().unwrap();
+        let x: i32 = cap[2].parse().unwrap();
+        let y: i32 = cap[3].parse().unwrap();
+        let w: i32 = cap[4].parse().unwrap();
+        let h: i32 = cap[5].parse().unwrap();
+
+        non_overlapped_ids.insert(id);
+        for i in x..(x + w) {
+            for j in y..(y + h) {
+                if grid[i as usize][j as usize] != 0 {
+                    non_overlapped_ids.remove(&last_drawn[i as usize][j as usize]);
+                    non_overlapped_ids.remove(&id);
+                }
+
+                grid[i as usize][j as usize] += 1;
+                last_drawn[i as usize][j as usize] = id;
+            }
+        }
+    }
+
+    let mut total_overlap = 0;
+    for row in grid.iter() {
+        for col_val in row.iter() {
+            if col_val > &1 {
+                total_overlap += 1;
+            }
+        }
+    }
+
+    assert!(non_overlapped_ids.len() == 1, "Should only have one left.");
+
+    println!("Total overlap: {}", total_overlap);
+    println!(
+        "Non-overlapping id: {}",
+        non_overlapped_ids.iter().next().unwrap()
+    );
+}
