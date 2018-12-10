5b8dac258e4e2d3013354320dfd6a4be7907dda1
Eric Frees <ericfrees@gmail.com>
2018-12-09 21:24:20 -0600

    Optimize day 5

--- src/solutions/day5.rs
+++ src/solutions/day5.rs
@@ -1,35 +1,25 @@
 extern crate adventlib;
 
-use std::collections::HashMap;
-
 pub fn solve() {
     println!("Day 5");
 
     let lines = adventlib::read_input_lines("day5input.txt");
-    let line = &lines[0];
+    let line: Vec<_> = lines[0].as_bytes().iter().map(|x| *x).collect();
 
-    let lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
-    let mut uppercase_map = HashMap::<char, char>::new();
-
-    // input is ASCII only
-    for c in lowercase_letters.chars() {
-        uppercase_map.insert(c, c.to_uppercase().next().unwrap());
-    }
-
-    let current_string = react_string(line, &uppercase_map);
+    let current_string = react_string(line);
 
     println!("Final length (part 1): {}", current_string.len());
 
     let mut min_length = current_string.len();
 
-    for filter_c in lowercase_letters.chars() {
-        let upper_filter_c = uppercase_map.get(&filter_c).unwrap();
-        let filtered_line: String = current_string
-            .chars()
-            .filter(|c| c != &filter_c && c != upper_filter_c)
+    for letter_idx in 0..26 {
+        let filtered_line: Vec<_> = current_string
+            .iter()
+            .filter(|c| **c != b'a' + letter_idx && **c != b'A' + letter_idx)
+            .map(|x| *x)
             .collect();
 
-        let reacted_string = react_string(&filtered_line, &uppercase_map);
+        let reacted_string = react_string(filtered_line);
 
         if reacted_string.len() < min_length {
             min_length = reacted_string.len();
@@ -39,39 +29,33 @@ pub fn solve() {
     println!("Best Length (part 2): {}", min_length);
 }
 
-fn react_string(line: &str, uppercase_map: &HashMap<char, char>) -> String {
+fn react_string(line: Vec<u8>) -> Vec<u8> {
     let mut replaced_one = true;
-    let mut current_string = line.to_string();
-    let mut new_string = String::with_capacity(current_string.len()/2);
+    let mut cur_string = line;
+    let mut new_string = Vec::with_capacity(cur_string.len() / 2);
 
     while replaced_one {
         replaced_one = false;
         let mut skip_one = false;
-        for (i, c) in current_string.char_indices() {
+        for i in 0..cur_string.len() {
             if skip_one {
                 skip_one = false;
                 continue;
             }
 
-            let next_char = current_string[i + 1..].chars().next();
-            match next_char {
-                Some(nextc) => {
-                    if (uppercase_map.contains_key(&c) && uppercase_map.get(&c).unwrap() == &nextc)
-                        || (uppercase_map.contains_key(&nextc)
-                            && uppercase_map.get(&nextc).unwrap() == &c)
-                    {
-                        skip_one = true;
-                        replaced_one = true;
-                    } else {
-                        new_string.push(c);
-                    }
-                }
-                None => new_string.push(c),
+            if i == cur_string.len() - 1 {
+                new_string.push(cur_string[i]);
+            } else if (cur_string[i] as i16 - cur_string[i + 1] as i16).abs() == 32 {
+                skip_one = true;
+                replaced_one = true;
+            } else {
+                new_string.push(cur_string[i]);
             }
         }
-        current_string = new_string;
-        new_string = String::with_capacity(current_string.len()/2);
+
+        cur_string = new_string;
+        new_string = Vec::with_capacity(cur_string.len() / 2);
     }
 
-    return current_string;
+    return cur_string;
 }
