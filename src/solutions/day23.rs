99dc8a27c5f43911fc9cad70d1586176f1edea13
Eric Frees <ericfrees@gmail.com>
2019-12-05 23:28:51 -0600

    Initial work on day 23, part 2

--- src/solutions/day23.rs
+++ src/solutions/day23.rs
@@ -5,24 +5,40 @@ use regex::Regex;
 pub fn solve() {
     println!("Day 23");
 
-    let lines = adventlib::read_input_lines("day23input.txt");
+    let lines = adventlib::read_input_lines("day23test2input.txt");
 
     let bots: Vec<_> = lines.iter().map(|l| parse_bot(l)).collect();
     let strongest_bot = bots.iter().max_by_key(|b| b.radius).expect("Must be a max");
 
-    let bots_in_reach = bots
-        .iter()
-        .filter(|b| {
-            b.location.manhattan_dist_to(&strongest_bot.location) <= strongest_bot.radius as i64
-        })
-        .count();
+    let bots_in_reach = bots_in_reach_of(strongest_bot, &bots);
 
     println!("Bots reached by strongest: {}", bots_in_reach);
+
+    let origin = Point3d::new(0,0,0);
+    let bots_connected = bots.iter().map(|b| reached_by_bot_count(b, &bots));
+
+    let mut max_bot_count = 0;
+    let mut max_bot_dist = !0;
+
+    for (i, count) in bots_connected.enumerate() {
+        if count >= max_bot_count {
+            let bot_dist = bots[i].location.manhattan_dist_to(&origin);
+
+            if count > max_bot_count || bot_dist < max_bot_dist {
+                max_bot_count = count;
+                max_bot_dist = bot_dist;
+            }
+        }
+    }
+
+    //> 112121559
+    println!("Maximally connected count: {}", max_bot_count);
+    println!("Distance to maximally connected: {}", max_bot_dist);
 }
 
 fn parse_bot(line: &str) -> NanoBot {
     lazy_static! {
-        static ref pattern: Regex =
+        static ref PATTERN: Regex =
             Regex::new(r"pos=<([-\d]+),([-\d]+),([-\d]+)>, r=(\d+)").expect("Parse pattern");
     }
     let captures = pattern.captures(line).expect("Line should match format");
@@ -35,6 +51,22 @@ fn parse_bot(line: &str) -> NanoBot {
         location: location,
         radius: captures[4].parse().unwrap(),
     }
+}
+
+fn bots_in_reach_of(bot: &NanoBot, all_bots: &Vec<NanoBot>) -> usize {
+    all_bots.iter()
+        .filter(|b| {
+            b.location.manhattan_dist_to(&bot.location) <= bot.radius as i64
+        })
+        .count()
+}
+
+fn reached_by_bot_count(bot: &NanoBot, all_bots: &Vec<NanoBot>) -> usize {
+    all_bots.iter()
+        .filter(|b| {
+            b.location.manhattan_dist_to(&bot.location) <= b.radius as i64
+        })
+        .count()
 }
 
 struct NanoBot {
